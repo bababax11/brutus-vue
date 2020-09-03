bdfb43a30d56bc9be84cf8fc84ed934ba876080b
Odagawa <xtctl.1234@gmail.com>
2020-09-03 19:54:39 +0900

    add message

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,8 @@
 <template>
   <main>
-    <div>
+    <div class="center">
       <!-- <TopPanel /> -->
+      <span>{{ message ? message : turn }}</span>
       <Board :game="game" @select="select" />
     </div>
   </main>
@@ -16,7 +17,13 @@ export default {
   data() {
     return {
       game: new GameState(),
-      selected: null
+      selected: null,
+      message: ""
+    }
+  },
+  computed: {
+    turn() {
+      return this.game.turn === 1 ? "先手番です" : "後手番です"
     }
   },
   methods: {
@@ -32,11 +39,10 @@ export default {
         return
       }
       const d = [i - si, j - sj]
-      // let state
+      let state
       try {
         console.log(si, sj, d)
-        this.game.moveDVec(si, sj, d)
-        console.log(this.game.board)
+        state = this.game.moveDVec(si, sj, d)
         this.$forceUpdate()
         console.log(this.game.board)
       } catch (e) {
@@ -46,13 +52,17 @@ export default {
         return
       }
       this.selected = null
+      if (state === 1) {
+        this.message = "先手勝利です"
+      } else if (state === -1) {
+        this.message = "後手勝利です"
+      }
     }
   }
 }
 </script>
 <style lang="scss" scoped>
-// main {
-//   display: flex;
-//   padding: 40 10;
-// }
+.center {
+  text-align: center;
+}
 </style>
